e558367dcde0bcbe13e2afbafe21790323f824dc
Cm-15 <96298282+Cm-15@users.noreply.github.com>
2025-09-07 19:39:25 +0700

    Update main_s_0489_W9.py

--- W9/main_s_0489_W9.py
+++ W9/main_s_0489_W9.py
@@ -1,2 +1,15 @@
+import sys
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        print(f"Usage: python {sys.argv[0]} <number1> <number2>")
+        return
+    try:
+        num1 = int(sys.argv[1])
+        num2 = int(sys.argv[2])
+        result = num1 + num2
+        print(f"ผลรวมคือ: {result}")
+    except ValueError:
+        print("ValueError")
+if __name__ == "__main__":
+    main()
